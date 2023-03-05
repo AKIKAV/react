df7dcfe93a9580b5348fe4438b082284a54a08db
AKIKAV <120468339+AKIKAV@users.noreply.github.com>
2023-03-05 21:13:32 +0530

    Add files via upload

---
+++ Components/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu=()=>{
+    return(
+        
+        <ul className="main">
+            <li><a href="">Home</a></li>
+            <li><a href="">Login</a></li>
+            <li><a href="">About</a></li>
+        </ul>
+        
+    )
+}
+export default Menu
---
+++ Components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from './Logo';
+import Menu from './Menu';
+ 
+const  Nav=()=>{
+    return(
+        
+        <nav className="nav">
+        <Logo/>
+        <Menu/> 
+        </nav>
+        
+    )
+}
+
+export default Nav
